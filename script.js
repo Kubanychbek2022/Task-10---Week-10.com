0e7bc78465b3978ffd3f60f382090d3f996dc542
Kubanychbek Tursunbekov <kubanychbek.tursunbekov.alt@gmail.com>
2022-07-29 21:12:18 +0300

    add all

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-// window.addEventListener("load", function () {
-//   const title = this.document
-// })
+window.addEventListener("load", function () {
+  const title = document.querySelectorAll(".accordeon__title");
+
+
+  for(let i = 0; i < title.length; i++) {
+    title[i].addEventListener("click", function () {
+      this.classList.toggle("minus");
+      this.nextElementSibling.classList.toggle("text");
+      this.nextElementSibling.classList.toggle("text-underline");
+    })
+  }
+
+});
